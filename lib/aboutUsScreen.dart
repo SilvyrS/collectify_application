fb3178ad430c07d3108ddaf44ab94dc8969aebbc
Jingkay Simbran <angelicagrace.simbran@wvsu.edu.ph>
2023-02-20 17:58:56 +0800

    Add text

--- lib/aboutUsScreen.dart
+++ lib/aboutUsScreen.dart
@@ -32,7 +32,29 @@ class AboutUs extends StatelessWidget {
               width: 250,
               height: 150,
             ),
-          ])
+          ]),
+          RichText(
+            text: TextSpan(
+              text: 'Collectify is ',
+              style: TextStyle(
+                fontSize: 40,
+                color: Colors.yellow,
+                decoration: TextDecoration.underline,
+                decorationStyle: TextDecorationStyle.double,
+              ),
+              children: <TextSpan>[
+                TextSpan(
+                  text:
+                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.black,
+                    decoration: TextDecoration.none,
+                  ),
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
