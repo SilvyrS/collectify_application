5f7b53fc73ccdeb6965d3f2a0a9e0264b3d9cd71
SilvyrS <tigershadows09@gmail.com>
2023-02-21 17:06:01 +0300

    Font and Alignment
    Changed font of description and justified the text.
    
    Removed: Double underline -> single underline

--- lib/aboutUsScreen.dart
+++ lib/aboutUsScreen.dart
@@ -34,6 +34,7 @@ class AboutUs extends StatelessWidget {
             ),
           ]),
           Container(
+<<<<<<< Updated upstream
             color: Color(0xFFbbdc59),
             child: Padding(
               padding: const EdgeInsets.fromLTRB(50, 20, 50, 120),
@@ -58,6 +59,34 @@ class AboutUs extends StatelessWidget {
                     )
                   ],
                 ),
+=======
+            color: Color.fromARGB(255, 211, 230, 156),
+            margin: EdgeInsets.fromLTRB(50, 10, 50, 0),
+            //child: Padding(
+            //padding: const EdgeInsets.fromLTRB(50, 20, 50, 50),
+            child: RichText(
+              textAlign: TextAlign.justify,
+              softWrap: true,
+              text: TextSpan(
+                text: 'Collectify',
+                style: GoogleFonts.lobsterTwo(
+                  fontSize: 30,
+                  color: Color(0xFF30326b),
+                  decoration: TextDecoration.underline,
+                  decorationStyle: TextDecorationStyle.solid,
+                ),
+                children: <TextSpan>[
+                  TextSpan(
+                    text:
+                        ' is a gallery application showcasing a collection of faunas around the world. With its user-friendly interface, Collectify enables users to explore and learn about these creatures through vivid images, detailed descriptions, and engaging facts. Whether you are a seasoned naturalist or simply curious about the animal kingdom. Collectify offers an immersive and informative experience that is both fun and educational.',
+                    style: GoogleFonts.robotoSlab(
+                      fontSize: 15,
+                      color: Colors.black,
+                      decoration: TextDecoration.none,
+                    ),
+                  )
+                ],
+>>>>>>> Stashed changes
               ),
             ),
           )
