996c7807ed9a9e179209b82443ea75352ece32dc
Jingkay Simbran <angelicagrace.simbran@wvsu.edu.ph>
2023-04-11 14:55:36 +0800

    Create new file app_drawer and create a drawer

--- lib/aboutUsScreen.dart
+++ lib/aboutUsScreen.dart
@@ -1,7 +1,10 @@
+import 'package:collectify_application/app_drawer.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class AboutUs extends StatelessWidget {
+  const AboutUs({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,62 +13,71 @@ class AboutUs extends StatelessWidget {
         title: Text('About Us', style: TextStyle(color: Color(0xFF7FFEBAD))),
       ),
       body: Column(
-        children: [
-          Row(
-              mainAxisAlignment:
-                  MainAxisAlignment.center, //Center Column contents vertically,
-              children: [
-                Image.asset(
-                  '../assets/logo1.png',
-                  width: 150,
-                  height: 150,
-                  fit: BoxFit.contain,
-                ),
-                Text(
-                  'Collectify',
-                  style: GoogleFonts.lobsterTwo(fontSize: 60),
-                ),
-              ]),
-          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Image.asset(
-              '../assets/color_paletter.jpg',
-              width: 250,
-              height: 150,
-            ),
-          ]),
-          Container(
-            color: Color.fromARGB(255, 211, 230, 156),
-            margin: EdgeInsets.fromLTRB(30, 10, 30, 0),
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(50, 20, 50, 50),
-              child: RichText(
-                textAlign: TextAlign.justify,
-                textDirection: TextDirection.ltr,
-                softWrap: true,
-                text: TextSpan(
-                  text: 'Collectify',
-                  style: GoogleFonts.lobsterTwo(
-                    fontSize: 30,
-                    color: Color(0xFF30326b),
-                    decoration: TextDecoration.underline,
-                    decorationStyle: TextDecorationStyle.solid,
-                  ),
-                  children: <TextSpan>[
-                    TextSpan(
-                      text:
-                          ' is a gallery application showcasing a collection of faunas around the world. With its user-friendly interface, Collectify enables users to explore and learn about these creatures through vivid images, detailed descriptions, and engaging facts. Whether you are a seasoned naturalist or simply curious about the animal kingdom. Collectify offers an immersive and informative experience that is both fun and educational.',
-                      style: GoogleFonts.robotoSlab(
-                        fontSize: 15,
-                        color: Colors.black,
-                        decoration: TextDecoration.none,
-                      ),
-                    )
-                  ],
-                ),
+        children: [AppTitle(), AppDescription()],
+      ),
+      drawer: MyAppBar(),
+    );
+  }
+
+  Column AppTitle() {
+    return Column(
+      children: [
+        Row(
+            mainAxisAlignment:
+                MainAxisAlignment.center, //Center Column contents vertically,
+            children: [
+              Image.asset(
+                '../assets/logo1.png',
+                width: 150,
+                height: 150,
+                fit: BoxFit.contain,
               ),
+              Text(
+                'Collectify',
+                style: GoogleFonts.lobsterTwo(fontSize: 60),
+              ),
+            ]),
+        Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Image.asset(
+            '../assets/color_paletter.jpg',
+            width: 250,
+            height: 150,
+          ),
+        ]),
+      ],
+    );
+  }
+
+  Container AppDescription() {
+    return Container(
+      margin: EdgeInsets.fromLTRB(30, 10, 30, 0),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(50, 20, 50, 50),
+        child: RichText(
+          textAlign: TextAlign.justify,
+          textDirection: TextDirection.ltr,
+          softWrap: true,
+          text: TextSpan(
+            text: 'Collectify',
+            style: GoogleFonts.lobsterTwo(
+              fontSize: 30,
+              color: Color(0xFF30326b),
+              decoration: TextDecoration.underline,
+              decorationStyle: TextDecorationStyle.solid,
             ),
-          )
-        ],
+            children: <TextSpan>[
+              TextSpan(
+                text:
+                    ' is a gallery application showcasing a collection of faunas around the world. With its user-friendly interface, Collectify enables users to explore and learn about these creatures through vivid images, detailed descriptions, and engaging facts. Whether you are a seasoned naturalist or simply curious about the animal kingdom. Collectify offers an immersive and informative experience that is both fun and educational.',
+                style: GoogleFonts.robotoSlab(
+                  fontSize: 15,
+                  color: Colors.black,
+                  decoration: TextDecoration.none,
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
---
+++ lib/app_drawer.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+Widget MyAppBar() {
+  return Drawer(
+    child: ListView(
+      children: <Widget>[
+        DrawerHeader(
+          decoration: const BoxDecoration(
+            color: Color(0xFF130f40),
+          ),
+          child: HeaderDrawer(),
+        ),
+        ListDrawer()
+      ],
+    ),
+  );
+}
+
+Column HeaderDrawer() {
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Image.asset(
+        '../assets/logo1.png',
+        height: 50,
+        width: 50,
+      ),
+      const SizedBox(
+        height: 20,
+      ),
+      Text(
+        'MENU',
+        style: TextStyle(color: Colors.white, fontSize: 20.0),
+      ),
+    ],
+  );
+}
+
+Column ListDrawer() {
+  return Column(
+    children: [
+      ListTile(
+        leading: Image.asset(
+          '../assets/home.png',
+          height: 30,
+          width: 30,
+        ),
+        title: Text('Home'),
+      ),
+      SizedBox(
+        height: 20,
+      ),
+      ListTile(
+        leading: Image.asset(
+          '../assets/animal.png',
+          height: 30,
+          width: 30,
+        ),
+        title: Text('Animal Kingdom'),
+      ),
+      SizedBox(
+        height: 20,
+      ),
+      ListTile(
+        leading: Image.asset(
+          '../assets/aboutus.png',
+          height: 30,
+          width: 30,
+        ),
+        title: Text('About Us'),
+      ),
+      SizedBox(
+        height: 500,
+      ),
+      Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [Text('All Rights Reserved.')],
+      ),
+    ],
+  );
+}
