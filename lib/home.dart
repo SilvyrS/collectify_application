1a6a67da8a9a0acd30c66ed51f814a72c8584cb3
SilvyrS <tigershadows09@gmail.com>
2023-04-24 10:02:46 +0300

    attempted the gridimage

--- lib/home.dart
+++ lib/home.dart
@@ -38,23 +38,68 @@ class HomeScreen extends StatelessWidget {
     );
   }
 
-  //
   Container GridImage() {
+    return Container(
+      child: ListView(
+        scrollDirection: Axis.vertical,
+        children: [
+          Container(
+            decoration: BoxDecoration(color: Colors.amber),
+            width: 100,
+            child: Row(
+              children: [Text('HI'), Image.asset('../assets/spyrotest.png')],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  /*Container GridImage() {
+    return Container(
+      child: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: [
+            Row(
+              children: [Text('HI'), Image.asset('../assets/spyrotest.png')],
+            ),
+            Row(
+              children: [Text('HI'), Image.asset('../assets/spyrotest.png')],
+            ),
+            Row(
+              children: [Text('HI'), Image.asset('../assets/spyrotest.png')],
+            ),
+            Row(
+              children: [Text('HI'), Image.asset('../assets/spyrotest.png')],
+            ),
+            Row(
+              children: [Text('HI'), Image.asset('../assets/spyrotest.png')],
+            ),
+          ],
+        ),
+      ),
+    );
+  }*/
+
+  //
+  /*Container GridImage() {
     return Container(
       child: Container(
         margin: EdgeInsets.all(10.0),
         child: GridView.count(
-            crossAxisCount: 2,
+            crossAxisCount: 1,
             crossAxisSpacing: 5.0,
             mainAxisSpacing: 10.0,
             shrinkWrap: true,
             children: <Widget>[
-              Image.asset('../assets/spyrotest.png'),
-              Image.asset('../assets/spyrotest.png'),
-              Image.asset('../assets/spyrotest.png'),
-              Image.asset('../assets/spyrotest.png'),
+              Container(
+                height: 100,
+                color: Color.fromARGB(255, 123, 163, 143),
+              ),
             ]),
       ),
     );
   }
+  */
 }
