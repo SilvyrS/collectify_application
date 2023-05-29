6b65cabe380b53d663a72093bd745e70c28f074e
SilvyrS <129019721+NicoBeth@users.noreply.github.com>
2023-05-29 08:31:31 +0300

    detials

--- lib/detailsPage.dart
+++ lib/detailsPage.dart
@@ -12,7 +12,7 @@ class DetailsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 224, 226, 219),
+      backgroundColor: Color(0xFFbbdc59),
       appBar: AppBar(
         centerTitle: true,
         backgroundColor: Color(0xFF130f40),
@@ -23,74 +23,143 @@ class DetailsPage extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Stack(
-            children: [
-              Positioned(
-                top: 0,
-                child: FutureBuilder<String>(
-                  future: _getImageUrl(item.onlineimgPath),
-                  builder: (context, snapshot) {
-                    if (snapshot.hasData) {
-                      return Image.network(snapshot.data!);
-                    } else if (snapshot.hasError) {
-                      return Text('Error loading image');
-                    } else {
-                      return CircularProgressIndicator();
-                    }
-                  },
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Container(
-                  height: 550,
-                  padding: EdgeInsets.all(20),
-                  decoration: BoxDecoration(
-                    color: Color(0xFF130f41),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Stack(
+                children: [
+                  FutureBuilder<String>(
+                    future: _getImageUrl(item.onlineimgPath),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        return Image.network(snapshot.data!);
+                      } else if (snapshot.hasError) {
+                        return Text('Error loading image');
+                      } else {
+                        return CircularProgressIndicator();
+                      }
+                    },
                   ),
-                  child: Container(
-                    padding: EdgeInsets.all(20),
-                    decoration: BoxDecoration(
-                      color: Color(0xFF6ab04d),
-                    ),
-                    child: Column(
-                      children: [
-                        Text(
-                          item.kingdom,
-                          style: GoogleFonts.roboto(
-                              color: Color.fromARGB(255, 255, 255, 255),
-                              fontSize: 30,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        Text(
-                          item.name,
-                          style: TextStyle(
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold,
-                              color: Color(0xFF130f41)),
-                        ),
-                        SizedBox(height: 100),
-                        Container(
-                            padding: EdgeInsets.all(10.0),
-                            decoration: BoxDecoration(
-                              color: Color(0xFFbbdc59),
-                            ),
-                            child: Text(
-                              item.description,
-                              style: TextStyle(
-                                fontSize: 16,
-                              ),
-                              textAlign: TextAlign.justify,
-                            )),
-                      ],
+                  Transform.translate(
+                    offset: Offset(0, 125),
+                    child: Center(
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            _buildDetails(context),
+                          ]),
                     ),
                   ),
-                ),
+                ],
               ),
-            ],
+            ),
           ),
         ],
       ),
+    );
+  }
+
+  Widget _buildDetails(BuildContext context) {
+    return Positioned(
+      top: 30,
+      child: Container(
+        padding: const EdgeInsets.all(0.0),
+        child: Container(
+          height: 550,
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+              color: Color(0xFF6ab04d),
+              borderRadius: BorderRadius.circular(20)),
+          child: Container(
+            padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+                color: Color(0xFF30326b),
+                borderRadius: BorderRadius.circular(20)),
+            child: Column(
+              children: [
+                _buildTopDetails(context),
+                SizedBox(
+                  height: 75,
+                  width: 250,
+                ),
+                Text(
+                  "Description",
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 255, 255, 255)),
+                ),
+                Container(
+                  width: 250,
+                  child: Text(
+                    item.description,
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(255, 255, 255, 255)),
+                    textAlign: TextAlign.justify,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTopDetails(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          item.name,
+          style: GoogleFonts.roboto(
+              color: Color.fromRGBO(255, 255, 255, 1),
+              fontSize: 30,
+              fontWeight: FontWeight.bold),
+        ),
+        Text(
+          item.name,
+          style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Color.fromARGB(255, 255, 255, 255)),
+        ),
+        Text(
+          item.name,
+          style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Color.fromARGB(255, 255, 255, 255)),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildBotDetails(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          item.kingdom,
+          style: GoogleFonts.roboto(
+              color: Color.fromRGBO(255, 255, 255, 1),
+              fontSize: 30,
+              fontWeight: FontWeight.bold),
+        ),
+        Text(
+          item.name,
+          style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Color.fromARGB(255, 255, 255, 255)),
+        ),
+        Text(
+          item.genus,
+          style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Color.fromARGB(255, 255, 255, 255)),
+        ),
+      ],
     );
   }
 
